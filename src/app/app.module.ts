06ed5fbae6be8b12761dba3aa548198cc4845dfe
Norman Dankert <norman.dankert@daimler.com>
2018-06-14 16:02:26 +0200

    add library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyLibModule } from 'my-lib';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
